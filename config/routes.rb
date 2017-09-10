d6291c611c72a6fcc9f3623c00ecf92f24068fea
vishal5963 <malukanivishal1992@gmail.com>
2017-09-10 10:54:20 +0530

    Routes for home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+
   devise_for :users do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     get "/", :to => "devise/sessions#new"
   end
   # root to: "devise/sessions#new"
+  root :to => redirect("/users/sign_in")
 end
